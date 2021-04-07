36f29983ea741bb68a0f4d2a078864d2c71af4bb
lekhanhduylam <lekhanhduylam@gmail.com>
2021-04-07 16:58:29 +0700

    setup build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAIwT0OqEDEfQ7M40dVJugdgPATBnjTmiU',
+    authDomain: 'angular-starter-c0108.firebaseapp.com',
+    projectId: 'angular-starter-c0108',
+    storageBucket: 'angular-starter-c0108.appspot.com',
+    messagingSenderId: '950621388475',
+    appId: '1:950621388475:web:bf052f8ba52e4e6aeaf39e',
+    measurementId: 'G-ZZCZBGD2RR',
+  },
 };
